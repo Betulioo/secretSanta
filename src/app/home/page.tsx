6b974421ceafc69338613abc6fd594bd141094cc
Betulioo <luisnunezmatute@gmail.com>
2024-11-10 23:04:41 +0100

    creating the structure

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1 className='text-[#D8F000FF] text-4xl'>Welcome to Secret Santa</h1>
+            <p>Get ready to exchange gifts with your friends and family!</p>
+        </div>
+    );
+};
+
+export default HomePage;
